6c6862556e5b273d10b759532d9321dadea85a2f
Alberto Basanta <dasantades@gmail.com>
2025-02-26 09:17:22 +0100

    CRUD ok

--- controllers/ubicacionesController.js
+++ controllers/ubicacionesController.js
@@ -6,7 +6,7 @@ class ubicacionesController {
 
     async create(req, res) {
         try {
-            const data = ubicacionesModel.create(req.body);
+            const data = await ubicacionesModel.create(req.body);
             res.status(201).json(data);
         } catch (error) {
             res.status(500).json({status: "error"});
@@ -14,28 +14,35 @@ class ubicacionesController {
     }
     async update(req, res) {
         try {
-            res.status(201).json({status: "ok"});
+            const id = req.params.id;
+            const data = await ubicacionesModel.update(id, req.body);
+            res.status(200).json(data);
         } catch (error) {
             res.status(500).json({status: "error"});
         }
     }
     async delete(req, res) {
         try {
-            res.status(201).json({status: "ok"});
+            const id = req.params.id;
+            const data = await ubicacionesModel.delete(id);
+            res.status(206).json(data);
         } catch (error) {
             res.status(500).json({status: "error"});
         }
     }
     async getOne(req, res) {
         try {
-            res.status(201).json({status: "ok"});
+            const id = req.params;
+            const data = await ubicacionesModel.getOne(id);
+            res.status(201).json(data);
         } catch (error) {
             res.status(500).json({status: "error"});
         }
     }
     async getAll(req, res) {
         try {
-            res.status(201).json({status: "ok"});
+            const data = await ubicacionesModel.getAll();
+            res.status(201).json(data);
         } catch (error) {
             res.status(500).json({status: "error"});
         }
--- models/ubicaciones.js
+++ models/ubicaciones.js
@@ -1,10 +1,30 @@
 import dbClient from "../config/dbClient.js";
+import { ObjectId } from "mongodb";
 
 class ubicacionesModel {
-    async create() {
+    async create(ubicacion) {
         const colUbicaciones = dbClient.db.collection('ubicaciones');
-        await colUbicaciones.insertOne(ubicacion);
- 
+        return await colUbicaciones.insertOne(ubicacion);
+    }
+
+    async update(id, ubicacion) {
+        const colUbicaciones = dbClient.db.collection('ubicaciones');
+        return await colUbicaciones.updateOne({ _id: new ObjectId(id) }, { $set: ubicacion });
+    }
+
+    async delete(id) {
+        const colUbicaciones = dbClient.db.collection('ubicaciones');
+        return await colUbicaciones.deleteOne({ _id: new ObjectId(id) });
+    }
+
+    async getAll() {
+        const colUbicaciones = dbClient.db.collection('ubicaciones');
+        return await colUbicaciones.find({}).toArray();
+    }
+
+    async getOne(id) {
+        const colUbicaciones = dbClient.db.collection('ubicaciones');
+        return await colUbicaciones.findOne({ _id: new ObjectId(id) });
     }
 }
 
