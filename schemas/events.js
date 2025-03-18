e0dae0197fca989723792d9204d1f7458fd0a8a0
Alberto Basanta <dasantades@gmail.com>
2025-03-18 13:23:19 +0100

    updated schema events

--- schemas/events.js
+++ schemas/events.js
@@ -6,10 +6,19 @@ const eventsSchema = new mongoose.Schema(
         start: { type: Date, required: true },
         description: { type: String },
         location: { type: mongoose.Schema.Types.ObjectId, ref: 'Ubicacion' }, // Relación con Ubicaciones
-        category: { type: [String], required: true }
+        category: {
+            type: [String], // Array de strings
+            required: true,
+            enum: ["clase", "ruta", "reunion", "evento"], // Solo permite estas opciones
+          },
+          level: {
+            type: [String], // Array de strings
+            enum: ["Iniciación", "Intermedio", "Avanzado"], // Solo permite estas opciones
+          }
     },{
-        timestamps: true
-    }
+        timestamps: true,
+      }
 );
 
 export default mongoose.model("events", eventsSchema);
+
